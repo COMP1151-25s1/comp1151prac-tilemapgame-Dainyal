028b8eb3915dfd5f1e35d89d38d1c45f1063bb20
dainyal1 <danielhongning@gmail.com>
2025-05-05 10:16:15 +1000

    Fixed player prefab and added player movement, wall collision

--- Assets/TilemapGame/tilesheet.tsx
+++ Assets/TilemapGame/tilesheet.tsx
@@ -1,4 +1,21 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="tinydungeon_tilesheet" tilewidth="16" tileheight="16" tilecount="132" columns="12">
  <image source="../kenney_tiny-dungeon/Tilemap/tilemap_packed.png" width="192" height="176"/>
+ <tile id="40">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16.3636" height="16"/>
+   <object id="2" x="0" y="0" width="16" height="16"/>
+   <object id="3" x="0" y="0" width="16" height="16"/>
+   <object id="4" x="0" y="0" width="16" height="16"/>
+   <object id="5" x="0" y="0" width="16" height="16"/>
+   <object id="6" x="0" y="0" width="16" height="16"/>
+   <object id="7" x="0" y="0" width="16" height="16"/>
+   <object id="8" x="0" y="0" width="16" height="16"/>
+   <object id="9" x="0" y="0" width="16" height="16"/>
+   <object id="10" x="0" y="0" width="16" height="16"/>
+   <object id="11" x="0" y="0" width="16" height="16"/>
+   <object id="12" x="0" y="0" width="16" height="16"/>
+   <object id="13" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
 </tileset>
