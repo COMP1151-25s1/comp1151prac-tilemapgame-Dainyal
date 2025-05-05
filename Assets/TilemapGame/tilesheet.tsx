c3c70ced2e4c4efc869497f1e074dac8df44bcec
dainyal1 <danielhongning@gmail.com>
2025-05-05 10:41:22 +1000

    created hazard collision and alerts

--- Assets/TilemapGame/tilesheet.tsx
+++ Assets/TilemapGame/tilesheet.tsx
@@ -18,4 +18,24 @@
    <object id="13" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
+ <tile id="61">
+  <properties>
+   <property name="unity:IsTrigger" value="true"/>
+   <property name="unity:layer" value="Hazard"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="1" width="14" height="14">
+    <properties>
+     <property name="unity:IsTrigger" value="true"/>
+     <property name="unity:layer" value="Hazard"/>
+    </properties>
+   </object>
+   <object id="2" x="1" y="1" width="14" height="14"/>
+   <object id="3" x="1" y="1" width="14" height="14">
+    <properties>
+     <property name="unity:IsTrigger" value="true"/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
